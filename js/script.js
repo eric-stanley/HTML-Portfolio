f795c01076e6c333a258c3ec9c0b0b87ebdb9fbd
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2020-02-17 00:05:42 +0530

    nav bar overflow changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 document.querySelector('.nav__btn').addEventListener('click', showNav);
+const links = document.querySelectorAll('.nav__single-link');
+
+for (const link of links) {
+ link.addEventListener('click', showNav);
+}
 
 function showNav() {
  document.querySelector('.nav__links').classList.toggle('show__links');
